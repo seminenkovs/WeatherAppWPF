cc68b1a441a0e17ab2dd9ad92795da75126626aa
Vitalii <seminenkovs@gmail.com>
2024-01-21 18:20:54 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WeatherAppWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
