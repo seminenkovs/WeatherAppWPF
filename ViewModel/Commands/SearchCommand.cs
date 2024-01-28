8d50eaa5b023631a45796fc8d15c98c16a692bf0
Vitalii <seminenkovs@gmail.com>
2024-01-28 12:11:27 +0100

    Created SearchCommand

---
+++ ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace WeatherAppWPF.ViewModel.Commands;
+
+public class SearchCommand : ICommand
+{
+    public WeatherVM VM { get; set; }
+
+    public event EventHandler? CanExecuteChanged;
+
+
+    public SearchCommand(WeatherVM vm)
+    {
+        VM = vm;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        VM.MakeQuery();
+    }
+}
---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using WeatherAppWPF.Model;
+using WeatherAppWPF.ViewModel.Helpers;
+
+namespace WeatherAppWPF.ViewModel;
+
+public class WeatherVM : INotifyPropertyChanged
+{
+    private string _query;
+    private CurrentConditions _currentConditions;
+    private City _selectedCity;
+
+    public string Query
+    {
+        get { return _query; }
+        set
+        {
+            _query = value;
+            OnPropertyChanged("Query");
+        }
+    }
+
+    public CurrentConditions CurrentConditions
+    {
+        get { return _currentConditions; }
+        set
+        {
+            _currentConditions = value;
+            OnPropertyChanged("CurrentConditions");
+        }
+    }
+
+    public City SelectedCity
+
+    {
+        get { return _selectedCity; }
+        set
+        {
+            _selectedCity = value;
+            OnPropertyChanged("SelectedCity");
+        }
+    }
+
+    public WeatherVM()
+    {
+        if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+        {
+            SelectedCity = new City
+            {
+                LocalizedName = "Krakow"
+            };
+
+            CurrentConditions = new CurrentConditions
+            {
+                WeatherText = "Partly cloudy",
+                Temperature = new Temperature
+                {
+                    Metric = new Units
+                    {
+                        Value = 21
+                    }
+                }
+            };
+        }
+    }
+
+    public async void MakeQuery()
+    {
+        var cities = await AccuWeatherHelper.GetCities(Query);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
